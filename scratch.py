bcfbfc98dbe99e0f472281b5cf011d38bf818dc6
Rayjan Wilson <rayjan.wilson@gmail.com>
2013-04-05 00:31:40 -0800

    fiddled with lp filter length and merged the surface plot with the gradient plot for visual clarity

--- scratch.py
+++ scratch.py
@@ -10,8 +10,16 @@ x = profile1[:,0]
 surf = profile1[:,1]
 surfGradient = np.gradient(surf)
 
-FFT = abs(scipy.fft(surf))
+FFT = scipy.fft(surf)
 freqs = scipy.fftpack.fftfreq(surf.size, x[1]-x[0])
+
+lowPassFilter = FFT[:]
+for i in range(len(lowPassFilter)):
+    if i>=25:lowPassFilter[i]=0
+
+surfFiltered = scipy.ifft(lowPassFilter)
+surfFilteredGradient = np.gradient(surfFiltered)
+surfSeconOrderGradient = np.gradient(surfFilteredGradient)
 
 ### Plotting
 scatter = Scatter()
@@ -23,7 +31,7 @@ scatterPlot = Plot()
 scatterPlot.add(scatter)
 scatterPlot.xLabel = "X Data"
 scatterPlot.yLabel = "Height"
-scatterPlot.title = "Profile 1"
+scatterPlot.title = "Surface (raw)"
 
 layout = PlotLayout()
 layout.addPlot(scatterPlot)
@@ -33,7 +41,7 @@ layout.save("profile1.png")
 # now plot the gradient
 scatter.yValues = surfGradient
 scatterPlot.yLabel = "dHeight/dX"
-scatterPlot.title = "Profile 1 - Gradient"
+scatterPlot.title = "Surface - Gradient"
 
 layout = PlotLayout()
 layout.addPlot(scatterPlot)
@@ -43,19 +51,60 @@ layout.save("profile1_gradient.png")
 #now plot the fft of the surface
 line = Line()
 line.xValues = freqs
-line.yValues = FFT
+line.yValues = abs(FFT)
 
 linePlot = Plot()
 linePlot.add(line)
 linePlot.xLimits = (0, .5)
-linePlot.xLabel = "freqs"
+linePlot.xLabel = "Freqs [Hz]"
 linePlot.yLabel = "Amplitude"
-linePlot.title = "Profile 1 - FFT"
+linePlot.title = "Surface - FFT (zoomed)"
 
 layout = PlotLayout()
 layout.addPlot(linePlot)
-layout.plot()
+#layout.plot()
 layout.save("profile1_fft.png")
 #pylab.subplot(212)
 #pylab.plot(freqs,20*scipy.log10(FFT),'x')
 #pylab.show()
+
+# plot the filtered surface with the gradient
+scatter1 = Scatter()
+scatter1.xValues = x
+scatter1.yValues = surfFiltered
+scatter1.markerSize = 1
+scatter1.label = "Surface"
+scatter1.color = "black"
+
+scatter2 = Scatter()
+scatter2.xValues = x
+scatter2.yValues = surfFilteredGradient
+scatter2.label = "1st Derivative"
+scatter2.markerSize = 1
+scatter2.color = "green"
+
+scatterPlot = Plot()
+scatterPlot.add(scatter1)
+scatterPlot.add(scatter2)
+
+scatterPlot.xLabel = "Distance Along Cross Section"
+scatterPlot.yLabel = "Surface Height"
+scatterPlot.setTwinX("1st Derivative", 1)
+scatterPlot.title = "Surface (filtered)"
+scatterPlot.hasLegend()
+
+layout = PlotLayout()
+layout.addPlot(scatterPlot)
+layout.plot()
+layout.save("profile1_filtered.png")
+
+
+# now plot the second order gradient of filtered surface
+#scatter.yValues = surfSeconOrderGradient
+#scatterPlot.yLabel = "dHeight^2/dX^2"
+#scatterPlot.title = "Surface - 2nd Gradient (filtered)"
+
+#layout = PlotLayout()
+#layout.addPlot(scatterPlot)
+#layout.plot()
+#layout.save("profile1_so_gradient_filtered.png")
