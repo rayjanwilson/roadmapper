f6c3fdfa8a9c35b38287559709f90f38ea8deebb
Rayjan Wilson <rayjan.wilson@gmail.com>
2013-04-12 13:04:45 -0800

    continue work on ANN implementation

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-roadmapper
+Roadmapper
 ==========
 
-a sketch for the bp road map thingy
+A sketch for the bp road map thingy. Some attempts at finding the road profile features (road edge, tow, etc).
+
+==========
+
+Installation instructions
+
+```bash
+pip install numpy scipy matplotlib pybrain
+```
--- nn_scratch.py
+++ nn_scratch.py
@@ -1,12 +1,45 @@
 #!/usr/bin/env python
 
 import numpy as np
-from pybrain.tools.shortcuts import buildNetwork
+#from pybrain.tools.shortcuts import buildNetwork
+from pybrain.structure import RecurrentNetwork
+from pybrain.structure import FullConnection
+from pybrain.structure import LinearLayer, SigmoidLayer
+from pybrain.datasets import SupervisedDataSet
+from pybrain.supervised.trainers import BackpropTrainer
 #from boomslang import Line, Scatter, Plot, PlotLayout
 
-profile1 = np.genfromtxt("./assets/profile3.txt")
-x = profile1[:, 0]
-surf = profile1[:, 1]
+#profile1 = np.genfromtxt("./assets/profile3.txt")
+#x = profile1[:, 0]
+#surf = profile1[:, 1]
 
-net = buildNetwork(2, 3, 1)
-net.activate([2, 1])
+#net = buildNetwork(2, 3, 1)
+#n = buildNetwork(2, 3, 1, bias=True)
+
+# from http://en.wikibooks.org/wiki/Artificial_Neural_Networks/Neural_Network_Basics
+no_hiddenLayer = 8
+learningRate = 0.3
+momentum = 0.05
+tainingType = 1 #train by minimum error
+minimumError = 0.01
+
+n = RecurrentNetwork()
+
+n.addInputModule(LinearLayer(2, name='in'))
+n.addModule(SigmoidLayer(3, name='hidden'))
+n.addOutputModule(LinearLayer(1, name='out'))
+n.addConnection(FullConnection(n['in'], n['hidden'], name='c1'))
+n.addConnection(FullConnection(n['hidden'], n['out'], name='c2'))
+
+ds = SupervisedDataSet(2, 1)
+ds.addSample((0, 0), (0,))
+ds.addSample((0, 1), (1,))
+ds.addSample((1, 0), (1,))
+ds.addSample((1, 1), (0,))
+
+#trainer = BackpropTrainer(n, ds)
+#trainer.trainUntilConvergence()
+
+n.sortModules()
+
+print net.activate([0,1])
